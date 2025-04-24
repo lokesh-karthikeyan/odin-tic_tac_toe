e3a693bf3d6279535fd26760e2560295bc3f02dc
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-04-24 22:47:39 +0530

    Feat: Add `GameBoard` IIFE module

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Singleton for Game Board object, returns current board state & method to update board.
+
+const GameBoard = (function () {
+  let board = new Array(9).fill("");
+
+  const getBoard = () => {
+    return board;
+  };
+
+  const updateBoard = (index, marker) => {
+    board[index] = marker;
+  };
+
+  return { getBoard, updateBoard };
+})();
